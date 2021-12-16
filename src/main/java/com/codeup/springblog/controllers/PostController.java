af2790978503f503aa43491148a4ab23d41f681b
Steven Lacy <steven.lacy11@gmail.com>
2021-12-16 15:19:42 -0600

    Completed PostController

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,7 +1,38 @@
 package com.codeup.springblog.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
+
+	@RequestMapping(path = "/posts", method = RequestMethod.GET)
+	@ResponseBody
+	public String posts() {
+		return "POST INDEX PAGE";
+	}
+
+	@RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public String postByID(@PathVariable long id) {
+		return "View an individual post " + id;
+	}
+
+	@RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+	@ResponseBody
+	public String postCreateGet() {
+		return "view the form for creating a post";
+	}
+
+	@RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+	@ResponseBody
+	public String postCreatePost() {
+		return "create a new post";
+	}
+
+
 }
