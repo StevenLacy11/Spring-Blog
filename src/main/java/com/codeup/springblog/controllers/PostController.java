1b8c25e512acbab72c1b51ac22f39230a6dd38a5
Steven Lacy <steven.lacy11@gmail.com>
2022-01-09 08:31:33 -0600

    Set up lecture emailServices.

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -9,11 +9,13 @@ public class PostController {
 
 	private final PostRepository postDao;
 	private final UserRepository userDao;
+	private final EmailService emailService;
 
 
-	public PostController(PostRepository postDao, UserRepository userDao){
+	public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService){
 		this.postDao = postDao;
 		this.userDao = userDao;
+		this.emailService = emailService;
 	}
 
 
